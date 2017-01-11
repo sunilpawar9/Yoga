bf0d3c96f1925fa04295e7c47a2ac1d3083f7415
Sunil Pawar <sunilpawar@nineinfosys.com>
2017-01-11 18:15:16 +0530

    Data uploaded code by supriya

--- app/src/main/java/com/geniusnine/android/yoga/MainActivity.java
+++ app/src/main/java/com/geniusnine/android/yoga/MainActivity.java
@@ -2,12 +2,35 @@ package com.geniusnine.android.yoga;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.client.Firebase;
 
 public class MainActivity extends Activity {
 
+    private Button AddData;
+    private Firebase Reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        Reference=new Firebase("https://yoga-1ea38.firebaseio.com/");
+
+        AddData=(Button)findViewById(R.id.btnadddata);
+        AddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Firebase RefChild=Reference.child("Name");
+
+                RefChild.setValue("Supriya");
+            }
+        });
     }
+
+
+
+
+
 }
